6bd2378e67bbc234c502d98fc6d8117871fe5abd
Nitzan Rosen <nitzan.rosen@mail.huji.ac.il>
2021-10-25 13:52:57 +0300

    add comments and minor changes

--- meturment_dvice.py
+++ meturment_dvice.py
@@ -1,64 +1,83 @@
 import nidaqmx
 import nidaqmx.system
+from nidaqmx.stream_readers import CounterReader
 from nidaqmx.constants import (AcquisitionType, CountDirection, Edge,
                                READ_ALL_AVAILABLE, TaskMode, TriggerType)
-from nidaqmx.stream_readers import CounterReader
 import numpy as np
 from time import time
 import matplotlib.pyplot as plt
 import pickle
 from scipy.io import wavfile
 
+#finding the connections of instruments
 system = nidaqmx.system.System.local()
+#select device
 device = system.devices[1] if system.devices else None
 
-
 def turn_off_led():
+    """
+    write zero voltage to device
+    :return:
+    """
     with nidaqmx.Task() as tx:
         tx.ao_channels.add_ao_voltage_chan(device.name + "/ao0")
         tx.write(0, True)
 
 
-def take_measurements(signal: np.ndarray, sampling_rate: float):
-    t = signal.size / sampling_rate
+def take_measurements(signal: np.ndarray, sampling_rate: float,offset=3.):
+    """
+
+    :param signal:
+    :param sampling_rate:
+    :return:
+    """
+    duration = signal.size / sampling_rate
     with nidaqmx.Task() as rx, nidaqmx.Task() as tx, nidaqmx.Task() as clock:
+
+        # analog input channel
         rx.ai_channels.add_ai_current_chan(device.name + "/ai0")
         rx.timing.cfg_samp_clk_timing(sampling_rate, sample_mode=AcquisitionType.CONTINUOUS)
+
+        # analog output channel
         tx.ao_channels.add_ao_voltage_chan(device.name + "/ao0")
         tx.timing.cfg_samp_clk_timing(sampling_rate, sample_mode=AcquisitionType.CONTINUOUS)
+
+        #clock channel?
         # clock.di_channels.add_di_chan(device.name+"/port0")
         # clock.timing.cfg_samp_clk_timing(sumpeling_rate, sample_mode=AcquisitionType.CONTINUOUS)
+        tx.stop() # make sure tx stopped
+        tx.write(signal + offset) #write output signal
+
+        #read data from rx
+        vals = rx.read(signal.size)
         tx.stop()
-        tx.write(signal + 3)
-        # vals = [rx.read() for _ in range(100)]
-        vals = []
-        start_time = time()
-        tx.start()
-        while time() - start_time < t:
-            vals.append(rx.read())
+
+        tx.write([0, 0, 0], True) # turn off diode
         tx.stop()
-        tx.write([0, 0, 0], True)
-        tx.stop()
-        # print(f"{rx.read()=}")
-        # for s in signal+7:
-        #     tx.write(s)
+
     return vals
 
 
 def distance(x1):
-    w = 570
-    T = 5
-    rate = 10000
-    x0 = 20
-    dis = x1 - x0
-    filename = f"distance/distance{dis}"
-    sig = np.sin(w * np.linspace(0, T, T * rate) * 2 * np.pi) / 2
-    values = take_measurements(sig, rate)
-    plt.plot(values)
+    w = 570 #output frequency
+    TimeInterval = 5 #time interval
+    rate = 10000 #sampling rate
+    x0 = 20 # initial location of diode on axis
+    dis = x1 - x0 #
+    offset = 3
+    filename = f"distance/distance{dis}.pickle" #output filename
+    signal = np.sin(w * np.linspace(0, TimeInterval, TimeInterval * rate) * 2 * np.pi) / 2
+    values = take_measurements(signal, rate, offset)
+
+    plt.plot(np.arange(values.size)/rate, values)
+    plt.xlabel("Sec")
+    plt.ylabel("Amplitude")
     plt.show()
+    plt.savefig(f"distance/measureDistance{dis}.jpg")
+    #save data to file
     with open(filename, "wb+") as f:
-        pickle.dump({"signal": sig, "received": values, "interval": T, "rate": rate, "time": time(), "distance": dis},
-                    f)
+        pickle.dump({"offset_voltage": offset, "signal": signal, "received": values, "interval": TimeInterval,
+                     "rate": rate, "time": time(), "distance": dis}, f)
 
 
 def noise():
@@ -73,10 +92,11 @@ def noise():
     plt.plot(values)
     plt.show()
     with open(filename, "wb+") as f:
-        pickle.dump({"signal": sig, "received": values, "interval": T, "rate": rate, "time": time()}, f)
+        pickle.dump({"signal": sig, "received": values, "interval": T, "rate": rate,
+                     "time": time(), "fileName": filename}, f)
 
 
-def song(filename, output_name="out.wav"):
+def song(filename, output_name="out.wav"): #todo
     sample_rate, data = wavfile.read(filename)
     data = data[:, 0] / (2 ** 16)
     values = take_measurements(data[0:10 * sample_rate], sample_rate)
@@ -108,6 +128,5 @@ if __name__ == "__main__":
     # distance(564654654)
     song("Cat Ievan Polkka (320 kbps).wav", "cat320out.wav")
     # sample_rate, data = wavfile.read("Cat Ievan Polkka (320 kbps).wav", "cat320out.wav")
-
     # for dev in system.devices:
     #     print(f"{dev.name=}")
