c9eca4da63ff5c87b4628a4e097d100453164774
Nitzan Rosen <nitzan.rosen@mail.huji.ac.il>
2021-10-31 21:35:26 +0200

    curve fitting and formatting plot

--- distance/analizeDistanceData.py
+++ distance/analizeDistanceData.py
@@ -3,13 +3,17 @@ from time import time
 import matplotlib.pyplot as plt
 import pickle
 import os
+
+from matplotlib.ticker import LogFormatter, FormatStrFormatter, LogFormatterMathtext
 from scipy.io import wavfile
 from scipy.optimize import curve_fit
+
 
 def openPickleFile(openfile):
     with open(openfile, "rb") as file:
         object = pickle.load(file)
     return object
+
 
 def drawAllMeasurements(objectsLst):
     for obj in objectsLst:
@@ -21,65 +25,99 @@ def drawAllMeasurements(objectsLst):
     plt.show()
     # plt.savefig(f"distanceAllMeasures.jpg")
 
+
 def analizeDistEffect(objectsLst):
     amp = []
     dist = []
     for obj in objectsLst:
-        slicedData = obj.get("received")[10000:45000] #sliced received data
-        amp.append((max(slicedData) - min(slicedData))/2) #amplitude
-        dist.append(obj.get("distance")) #distance
+        slicedData = obj.get("received")[10000:45000]  # sliced received data
+        amp.append((max(slicedData) - min(slicedData)) / 2)  # amplitude
+        dist.append(obj.get("distance"))  # distance
 
-    plt.plot(dist,amp)
+    plt.plot(dist, amp)
     plt.xlabel("distance [CM]")
     plt.ylabel("Amplitude [V]")
     plt.show()
-    return dist,amp
+    return dist, amp
 
-def OnePartsTSquared(t, a, b, c):
-    t = t[t + b != 0]
-    res = a * (1 / ((t + b) ** 2)) + c
-    return res
 
-def OnePartsTSquared1(t: float, a, b, c):
-    if t!= 0:
+def openNoisePickle():
+    """
+    :return: the average noise amplitude
+    """
+    obj = openPickleFile("noise_cover_led.pickle")
+    objData = obj.get("received")  # sliced received data
+
+    maxValue = np.max(objData)
+    minValue = np.min(objData)
+    amp = ((max(objData) - min(objData)) / 2)  # amplitude
+
+    plt.plot(objData)
+    plt.plot(np.repeat(maxValue, len(objData)))
+    plt.plot(np.repeat(minValue, len(objData)))
+    ampVec = np.repeat(amp, len(objData))
+    plt.plot(ampVec)
+    plt.show()
+
+    return amp
+
+
+##    def OnePartsTSquared1(t: float, a, b, c):
+        # if t != 0:
+            # res = a * (1 / ((t + b) ** 2)) + c
+            # return res
+
+def test(dist, amp):
+    def OnePartsTSquared(t, a, b, c):
+        # t = t[t + b != 0]
         res = a * (1 / ((t + b) ** 2)) + c
         return res
-    return 0
 
-##
+    #test on generated data
+    # xdata = np.linspace(1, 4, 50)
+    # y = xdata**-2#OnePartsTSquared(xdata, 1, 0, 0)
+    # rng = np.random.default_rng(2)
+    # y_noise = 0.2 * rng.normal(size=xdata.size)
+    # ydata = y + y_noise
+    # plt.plot(xdata, y)
+    # plt.plot(xdata, ydata, 'b-', label='data')
 
-def test():
-    xdata = np.linspace(1, 4, 50)
-    y = OnePartsTSquared(xdata, 1, 0, 0)
-    rng = np.random.default_rng(2)
-    y_noise = 0.2 * rng.normal(size=xdata.size)
-    ydata = y + y_noise
-    plt.plot(xdata, y)
-    plt.plot(xdata, ydata, 'b-', label='data')
+    # popt, pcov = curve_fit(OnePartsTSquared, xdata, ydata)
+    # plt.plot(xdata, OnePartsTSquared(xdata, *popt))
+    # print("test on generated data:")
+    # print(pcov)
+    # print(f"a: {popt[0]}, b: {popt[1]}, c: {popt[2]}")
+    # plt.show()
 
-    popt, pcov = curve_fit(OnePartsTSquared, xdata, ydata)
-    plt.plot(xdata, OnePartsTSquared(xdata, *popt))
+    #curve fit on my data
+    ax: plt.Axes = plt.axes()
+    plt.scatter(dist, amp)
+    popt1, pcov1 = curve_fit(OnePartsTSquared, dist, amp, check_finite=False)
+    x_fit = np.linspace(min(dist), max(dist), len(dist)*10)
+    plt.plot(x_fit, OnePartsTSquared(x_fit, *popt1))
+    ax.yaxis.set_major_formatter(LogFormatterMathtext(subs='all'))
+    print("real data:")
+    print(pcov1)
+    print(f"a: {popt1[0]}, b: {popt1[1]}, c: {popt1[2]}")
     plt.show()
+
     # popt, pcov = curve_fit(func, xdata, ydata, bounds=(0, [3., 1., 0.5]))
     # plt.plot(xdata, func(xdata, *popt), 'g--',
     #          label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
-
 ##
 
 
-
 if __name__ == '__main__':
-    o = [openPickleFile(filename) for filename in os.listdir() if not
-    (filename.endswith(".py") or filename.endswith(".jpg") or filename.endswith(".txt"))]
+    o = [openPickleFile(filename) for filename in os.listdir() if not (filename.endswith(".py") or filename.endswith(".jpg") or filename.endswith(".txt"))]
     o.sort(key=(lambda x: x["distance"]))
-    drawAllMeasurements(o)
-    dist,amp = analizeDistEffect(o)
-    np.savetxt('test1.txt', amp)  # x,y,z equal sized 1D arrays
+    # drawAllMeasurements(o)
+    dist, amp = analizeDistEffect(o)
+
+    # o = openPickleFile("noise_cover_led.pickle")
+    # openNoisePickle()
+    # print("hi")
+
+    # np.savetxt('amp.txt', amp)  # x,y,z equal sized 1D arrays
+    # np.savetxt('dist.txt', dist)  # x,y,z equal sized 1D arrays
     # demoFunction(dist,amp)
-    # test()
-
-
-
-
-
-
+    test(dist, amp)
